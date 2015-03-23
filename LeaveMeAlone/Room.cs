9345f91f29dec42ca51310beecd9ecb7ce0a46c3
chb2mn <chb2mn@virginia.edu>
2015-03-23 16:10:08 -0400

    adding room

---
+++ LeaveMeAlone/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LeaveMeAlone
+{
+    class Room
+    {
+        public int id;
+        public String name;
+        public int cost;
+        public int level;
+        public Delegate run;
+        public Texture2D img;
+    }
+}
