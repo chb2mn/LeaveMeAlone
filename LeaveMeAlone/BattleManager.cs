5e1fe522bafd6ba4caac7dcad8cc27d04c0c4429
chb2mn <chb2mn@virginia.edu>
2015-03-24 16:44:56 -0400

    Continuing development of battle manager, not done yet

--- LeaveMeAlone/BattleManager.cs
+++ LeaveMeAlone/BattleManager.cs
@@ -20,6 +20,8 @@ namespace LeaveMeAlone
 
         private static Texture2D buttonLocPic;
         private static Rectangle[] buttonLoc = new Rectangle[4];
+        private static Text[] button_text = new Text[4];
+        
         private static Text damage_text;
         private static int textx;
         private static int texty;
@@ -29,9 +31,11 @@ namespace LeaveMeAlone
         private static bool defeat;
 
         private static bool menu_change_in_progress = false;
-        private static int state_delay_counter = 0;
+        private static int animation_counter = 0;
         private static int state = 0;
+        private static int hovered_enemy = -1;
         private static int targeted_enemy = -1;
+        private static Skill selected_skill;
         //0 == Basic Menu
         //1 == Skills Menu
         //2 == Bribe Menu
@@ -42,15 +46,19 @@ namespace LeaveMeAlone
         {
             int button_basex = 100;
             int button_basey = 350;
-            
+
             buttonLoc[0] = new Rectangle(button_basex, button_basey, 250, 50);
             buttonLoc[1] = new Rectangle(button_basex + 300, button_basey, 250, 50);
             buttonLoc[2] = new Rectangle(button_basex, button_basey + 60, 250, 50);
             buttonLoc[3] = new Rectangle(button_basex + 300, button_basey + 60, 250, 50);
 
             buttonLocPic = Content.Load<Texture2D>("buttonbase");
-            bossLoc = new Rectangle(650, 120, 100, 100);
+            for (int i = 0; i < 4; i++)
+            {
+                button_text[i].loadContent(Content);
+            }
 
+            bossLoc = new Rectangle(650, 120, 100, 100);
 
             int hero_basex = 50;
             int hero_basey = 150;
@@ -80,27 +88,27 @@ namespace LeaveMeAlone
             {
                 if (heroLoc[i].Contains(selectLocX, selectLocY))
                 {
-                    targeted_enemy = i;
+                    hovered_enemy = i;
                     any_target = true;
                     if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     {
-                        return targeted_enemy;
+                        return hovered_enemy;
                     }
-                }    
+                }
             }
             if (bossLoc.Contains(selectLocX, selectLocY))
             {
-                targeted_enemy = -2;
+                hovered_enemy = -2;
                 any_target = true;
                 if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                 {
-                    return targeted_enemy;
+                    return hovered_enemy;
                 }
 
             }
             if (!any_target)
             {
-                targeted_enemy = -1;
+                hovered_enemy = -1;
             }
             //return no target if no target has been selected
             return -1;
@@ -123,50 +131,146 @@ namespace LeaveMeAlone
             }
         }
 
-        public static void Draw(SpriteBatch spriteBatch)
+        private static void NewMenu(int menu)
         {
-            //Do Background drawing
-            for (int i = 0; i < 4; i++)
+            menu_change_in_progress = true;
+            if (victory)
             {
-                try
-                {
-                    if (i == targeted_enemy)
+                return;
+            }
+            switch (menu)
+            {
+                case 0:
+                    state = 0;
+                    button_text[0].changeMessage("Attack");
+                    button_text[1].changeMessage("Skills");
+                    button_text[2].changeMessage("Taunt");
+                    button_text[3].changeMessage("Bribe");
+                    break;
+                case 1:
+                    //Skills Menu
+                    state = 1;
+                    for (int i = 0; i < 4; i++)
                     {
-                        spriteBatch.Draw(heroes[i].sprite, heroLoc[i], Color.Violet);
+                        button_text[i].changeMessage = boss.selected_skills[i].name;
                     }
-                    else
+                    break;
+                case 2:
+                    //Bribe Menu
+                    state = 2;
+                    break;
+                default:
+                    //When will we need this?
                     {
-                        spriteBatch.Draw(heroes[i].sprite, heroLoc[i], Color.White);
+                        state = 0;
                     }
-                }
-                catch
-                {
-                    //dead animation
-                }
+                    break;
             }
         }
 
         public static void Update()
         {
+            //If the mouse is released we can continue taking new input
+            if (Mouse.GetState().LeftButton == ButtonState.Released)
+            {
+                menu_change_in_progress = false;
+            }
             switch (state)
             {
                 case 0:
-                    foreach (Rectangle button in buttonLoc)
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed && !menu_change_in_progress)
+                    {
+                        int selectLocX = Mouse.GetState().X;
+                        int selectLocY = Mouse.GetState().Y;
+                        if (buttonLoc[1].Contains(selectLocX, selectLocY))
+                        {
+                            //Go toSkill menu
+                            NewMenu(1);
+                        }
+                        else if (buttonLoc[3].Contains(selectLocX, selectLocY))
+                        {
+                            //Go to Bribe menu
+                            NewMenu(2);
+                        }
+                        else if (buttonLoc[0].Contains(selectLocX, selectLocY))
+                        {
+                            //TODO: need a way to select basic attack
+                            selected_skill = boss.skills[0];
+                            state = 5;
+                        }
+                        else if (buttonLoc[2].Contains(selectLocX, selectLocY))
+                        {
+                            //TODO: need a way to select taunt
+                            selected_skill = boss.skills[1];
+                            targeted_enemy = -2;
+                            state = 6;
+                        }
+                    }
+                    break;
+                case 1:
+                    //Skill Selection
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed && !menu_change_in_progress)
                     {
 
                     }
                     break;
-                case 1:
-                    break;
                 case 2:
+                    //Bribe Stuff
                     break;
                 case 5:
-                    Target();
+                    //Targetting
+
+                    //highlighted needs to be separate from targeted enemy because target ensure that we have clicked on something
+                    targeted_enemy = Target();
+                    if (targeted_enemy != -1)
+                    {
+                        state = 6;
+                    }
                     break;
                 case 6:
+                    //Attacking
+                    Attack(boss, selected_skill);
                     break;
                 case 10:
+                    //Enemy Turn
                     break;
+            }
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            //Do Background drawing
+            //Draw Heroes
+            for (int i = 0; i < 4; i++)
+            {
+                try
+                {
+                    if (i == hovered_enemy)
+                    {
+                        spriteBatch.Draw(heroes[i].sprite, heroLoc[i], Color.Violet);
+                        //status too
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(heroes[i].sprite, heroLoc[i], Color.White);
+                        //status too
+                    }
+                }
+                catch
+                {
+                    //dead/KO animation
+                }
+                spriteBatch.Draw(boss.sprite, bossLoc, Color.White);
+                //status too
+            }
+            //Draw Buttons
+            if (state < 2)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    spriteBatch.Draw(buttonLocPic, buttonLoc[i], Color.White);
+                    button_text[i].draw(spriteBatch, textx, texty);
+                }
             }
         }
     }
