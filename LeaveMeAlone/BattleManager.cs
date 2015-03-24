b72282f5a21bed65046782337cd7b5ebadc0a5f1
chb2mn <chb2mn@virginia.edu>
2015-03-24 12:20:06 -0400

    Started with the Battle Manager

--- LeaveMeAlone/BattleManager.cs
+++ LeaveMeAlone/BattleManager.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace LeaveMeAlone
 {
     class BattleManager
     {
         public static List<Character> heroes;
+        public static List<Rectangle> heroLoc;
         public static Character boss;
         public static Rectangle bossLoc;
         public static Texture2D back;
@@ -20,20 +22,50 @@ namespace LeaveMeAlone
         private static int texty;
 
         private static Text victory_text;
-        bool victory;
+        private static bool victory;
 
-        bool menu_change_in_progress = false;
-        int state_delay_counter = 0;
-        int state = 0; 
-
-
+        private static bool menu_change_in_progress = false;
+        private static int state_delay_counter = 0;
+        private static int state = 0; 
+        //0 == Basic
+        //1 == Skills
+        //2 == Bribe
+        //5 == targeting
+        //6 == attacking
+        //10 == Enemy turn
         public static void Attack(Character caster, Skill skill){
 
         }
 
-        public static void Target(Character caster)
+        public static void Target()
         {
+            int selectLocX = Mouse.GetState().X;
+            int selectLocY = Mouse.GetState().Y;
 
+            bool any_target = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (heroLoc[i].Contains(selectLocX, selectLocY))
+                {
+                    targeted_enemy = i;
+                    any_target = true;
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    {
+                        Attack();
+                    }
+                }    
+            }
+            if (bossLoc.Contains(selectLocX, selectLocY))
+            {
+                targeted_enemy = -2;
+                any_target = true;
+
+            }
+            if (!any_target)
+            {
+                targeted_enemy = -1;
+            }
         }
 
         public static void CheckVictoryDefeat()
@@ -43,12 +75,28 @@ namespace LeaveMeAlone
 
         public static void Draw(SpriteBatch spriteBatch)
         {
+            //Do Background drawing
 
         }
 
         public static void Update()
         {
-
+            switch (state)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 5:
+                    Target();
+                    break;
+                case 6:
+                    break;
+                case 10:
+                    break;
+            }
         }
     }
 }
