500e0bd7e9f7ebfeb9b8b39b4530679e248030d5
Kyle O'Donnell <kah.odonnell@gmail.com>
2015-03-24 12:13:34 -0400

    manager skeletons

---
+++ LeaveMeAlone/BattleManager.cs
@@ -0,0 +1,35 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace LeaveMeAlone
+{
+    class BattleManager
+    {
+        public static List<Character> Heroes;
+        public static Character Boss;
+        public static void UseSkill(Character caster, Skill skill)
+        {
+
+        }
+        public static void CheckVictoryDefeat()
+        {
+
+        }
+        protected static override void Update(GameTime gameTime)
+        {
+
+        }
+        protected static override void Draw(SpriteBatch spritebatch, GameTime gametime)
+        {
+
+        }
+    }
+}
---
+++ LeaveMeAlone/PartyManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeaveMeAlone
+{
+    class PartyManager
+    {
+        public static int PartyNum;
+        public static int ArmyNum;
+        public static Character CreateHero()
+        {
+
+        }
+        protected static override void Update(GameTime gameTime)
+        {
+
+        }
+        protected static override void Draw(SpriteBatch spritebatch, GameTime gametime)
+        {
+
+        }
+    }
+}
