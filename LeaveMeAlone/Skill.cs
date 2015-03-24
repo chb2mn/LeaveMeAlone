2d008e3a41f7ae11e89c46f543ffb8b39ba032b6
Mitchell Smith <zephyrthenoble@gmail.com>
2015-03-23 23:41:19 -0400

    Added Skill

---
+++ LeaveMeAlone/Skill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeaveMeAlone
+{
+    class Skill
+    {
+        public static int total;
+        public int id;
+        public string name;
+        public int energy;
+        public int cost;
+        public int level;
+        public int cooldown;
+        public int type;
+        public string description;
+        public Delegate run;
+        public Skill(string name, int energy, int cost, int level, int cooldown, int type, string description, Delegate run)
+        {
+            this.name = name;
+            this.energy = energy;
+            this.cost = cost;
+            this.level = level;
+            this.cooldown = cooldown;
+            this.type = type;
+            this.description = description;
+            this.run = run;
+        }
+    }
+}
