0acf1f9c4362e7b8b5e9e3b7414a99ec977bbf16
Mitchell Smith <zephyrthenoble@gmail.com>
2015-04-04 17:43:38 -0400

    Edited fonts to load into a Dictionary

--- LeaveMeAlone/Text.cs
+++ LeaveMeAlone/Text.cs
@@ -10,12 +10,24 @@ namespace LeaveMeAlone
 {
     public class Text
     {
-        protected static SpriteFont font;
+        public static Dictionary<string, SpriteFont> fonts = new Dictionary<string,SpriteFont>();
+        public SpriteFont font;
+        public Color color;
         public string message;
+
+        public static Color DEFAULT_COLOR = Color.Black;
 
         public Text(SpriteFont f, Color c, string msg="")
         {
             message = msg;
+            font = f;
+            color = c;
+        }
+        public Text(string msg = "")
+        {
+            message = msg;
+            font = fonts["Arial-12"];
+            color = DEFAULT_COLOR;
         }
 
         public void changeMessage(string msg) 
@@ -26,7 +38,10 @@ namespace LeaveMeAlone
         public static void loadContent(ContentManager content)
         {
             //loads font
-            font = content.Load<SpriteFont>("Arial");
+            fonts["Arial-12"] = content.Load<SpriteFont>("Fonts/Arial-12");
+            fonts["Arial-24"] = content.Load<SpriteFont>("Fonts/Arial-24");
+            fonts["BattleMenuText-12"] = content.Load<SpriteFont>("Fonts/BattleMenuText-12");
+            fonts["RetroComputer-12"] = content.Load<SpriteFont>("Fonts/RetroComputer-12");
         }
 
         public void draw(SpriteBatch sb, int x, int y)
@@ -34,7 +49,7 @@ namespace LeaveMeAlone
             //draws a string, params are your font, your message, position, and color
             if (font != null)
             {
-                sb.DrawString(font, message, new Vector2(x, y), Color.Black);
+                sb.DrawString(font, message, new Vector2(x, y), color);
             }
         }
         
